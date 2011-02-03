0cb8affa7556f7718914a621da4f163661476b26
medes.imps <medes.imps@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-02-03 10:14:14 +0000

    Initial import.

---
+++ trunk/model/trunk/org.imogene.model.core/src/org/imogene/model/core/impl/TextFieldImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Medes-IMPS 2011
+ *
+ * $Id$
+ */
+package org.imogene.model.core.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.imogene.model.core.ImogenePackage;
+import org.imogene.model.core.TextField;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Text Field</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class TextFieldImpl extends StringFieldImpl implements TextField {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Medes-IMPS 2011";
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected TextFieldImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ImogenePackage.Literals.TEXT_FIELD;
+	}
+
+} //TextFieldImpl
