84b2db7337a6e5e9e86c6e622719e0503338f96c
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-02-25 14:47:49 +0000

    -- Support for translatable TextFields

---
+++ dev-libs/trunk/org.imogene.lib.synchro.server/src/org/imogene/sync/server/localizedtext/LocalizedTextHibernateDao.java
@@ -0,0 +1,154 @@
+package org.imogene.sync.server.localizedtext;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.imogene.common.dao.criteria.HibernateDaoUtil;
+import org.imogene.common.dao.criteria.ImogJunction;
+import org.imogene.common.data.Synchronizable;
+import org.imogene.sync.localizedtext.LocalizedText;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ * Implements a Hibernate DAO for the LocalizedText 
+ * @author Medes-IMPS
+ */
+public class LocalizedTextHibernateDao extends HibernateDaoSupport
+		implements
+			LocalizedTextDao {
+
+	private Logger logger = Logger.getLogger("org.imogene.translatable.dao");
+
+	@SuppressWarnings("unused")
+	public List<Synchronizable> loadEntities() {
+		logger.debug("Loading all LocalizedTexts");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		return crit.list();
+	}
+
+	public List<Synchronizable> loadEntities(int arg0, int arg1, String arg2,
+			boolean arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	public List<Synchronizable> loadEntities(ImogJunction criterions) {
+		logger.debug("Loading all LocalizedTexts");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(HibernateDaoUtil.addImogJunction(criterions));
+		return crit.list();
+	}
+
+	@SuppressWarnings("unused")
+	public Synchronizable loadEntity(String entityId) {
+		logger.debug("Loading the LocalizedText with id '" + entityId + "'.");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(Restrictions.eq("id", entityId));
+		List resultList = crit.list();
+		if (resultList != null && resultList.size() == 1)
+			return (LocalizedText) resultList.get(0);
+
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	public List<Synchronizable> loadModified(Date date) {
+		logger.debug("Loading LocalizedTexts modified since '"
+				+ date.toString() + "'.");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(Restrictions.ge("uploadDate", date));
+		return crit.list();
+	}
+
+	@SuppressWarnings("unused")
+	public List<Synchronizable> loadModified(Date date, ImogJunction criterions) {
+		logger.debug("Loading LocalizedTexts modified since '"
+				+ date.toString() + "'.");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(HibernateDaoUtil.addImogJunction(criterions));
+		crit.add(Restrictions.ge("uploadDate", date));
+		return crit.list();
+	}
+
+	@SuppressWarnings("unused")
+	public Synchronizable loadModified(Date date, String entityId) {
+		logger.debug("Loading LocalizedText with id='" + entityId
+				+ "' if modified since '" + date.toString() + "'.");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(Restrictions.eq("id", entityId));
+		crit.add(Restrictions.ge("uploadDate", date));
+
+		List resultList = crit.list();
+		if (resultList != null && resultList.size() == 1)
+			return (LocalizedText) resultList.get(0);
+
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	public Synchronizable loadModified(Date date, ImogJunction criterions,
+			String entityId) {
+		logger.debug("Loading the LocalizedText with id '" + entityId
+				+ "' modified since '" + date + "'.");
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.add(HibernateDaoUtil.addImogJunction(criterions));
+		crit.add(Restrictions.ge("uploadDate", date));
+		crit.add(Restrictions.eq("id", entityId));
+
+		List resultList = crit.list();
+		if (resultList != null && resultList.size() == 1)
+			return (LocalizedText) resultList.get(0);
+
+		return null;
+	}
+
+	public void saveOrUpdate(Synchronizable entity) {
+		logger.debug("Saving or updating LocalizedText with id '"
+				+ entity.getId() + "'.");
+		getSession().beginTransaction();
+		getHibernateTemplate().saveOrUpdate(entity);
+		getSession().getTransaction().commit();
+	}
+
+	public void merge(Synchronizable entity) {
+		logger.debug("Merging LocalizedText with id '" + entity.getId() + "'.");
+		getSession().beginTransaction();
+		getHibernateTemplate().merge(entity);
+		getSession().getTransaction().commit();
+	}
+
+	@SuppressWarnings("unused")
+	public int countAll() {
+		Criteria crit = getSession().createCriteria(LocalizedText.class);
+		crit.setProjection(Projections.rowCount());
+		List list = crit.list();
+		return Integer.parseInt(list.get(0).toString());
+	}
+
+	public void delete(Synchronizable entity) {
+		logger.debug("Deleting LocalizedText with id '" + entity.getId() + "'.");
+		getHibernateTemplate().delete(entity);
+	}
+
+	public void deleteEntities() {
+		logger.debug("Deleting LocalizedTexts");
+		String hsql = "delete from Doctor";
+		Query query = getSession().createQuery(hsql);
+		query.executeUpdate();
+	}
+
+	public void clear() {
+		getHibernateTemplate().clear();
+	}
+
+	public void flush() {
+		getHibernateTemplate().flush();
+	}
+
+}
