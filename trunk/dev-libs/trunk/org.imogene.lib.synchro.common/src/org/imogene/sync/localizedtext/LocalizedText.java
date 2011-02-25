2dff500bdd2258e0f15c86f1de5e4e006a543ff4
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-02-25 14:47:30 +0000

    -- Support for translatable TextFields

---
+++ trunk/dev-libs/trunk/org.imogene.lib.synchro.common/src/org/imogene/sync/localizedtext/LocalizedText.java
@@ -0,0 +1,137 @@
+package org.imogene.sync.localizedtext;
+
+import java.util.Date;
+
+import org.imogene.common.data.Synchronizable;
+
+/**
+ * LocalizedText Bean Implementation
+ * @author Medes-IMPS
+ */
+public class LocalizedText implements Synchronizable {
+
+	/* Synchronizable properties */
+	private String id;
+	private Date modified;
+	private Date uploadDate;
+	private String modifiedBy;
+	private String modifiedFrom;
+	private Date created;
+	private String createdBy;
+
+	/* Entity properties */
+	private String fieldId;
+	private String locale;
+	private String value;
+
+	public LocalizedText() {
+
+		// Add the ENABLED keyword before 'START' to protect this area 
+		/*PROTECTED REGION ID(initTRSN) START*/
+		/*PROTECTED REGION END*/
+
+	}
+
+	/* Entity getters and setters */
+
+	public String getFieldId() {
+		return fieldId;
+	}
+
+	public void setFieldId(String param) {
+		fieldId = param;
+	}
+
+	public String getLocale() {
+		return locale;
+	}
+
+	public void setLocale(String param) {
+		locale = param;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String param) {
+		value = param;
+	}
+
+	/* Synchronizable getters and setters */
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String pId) {
+		id = pId;
+	}
+
+	public Date getModified() {
+		return modified;
+	}
+
+	public void setModified(Date pModified) {
+		modified = pModified;
+	}
+
+	public Date getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(Date date) {
+		uploadDate = date;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String id) {
+		modifiedBy = id;
+	}
+
+	public String getModifiedFrom() {
+		return modifiedFrom;
+	}
+
+	public void setModifiedFrom(String modifiedFrom) {
+		this.modifiedFrom = modifiedFrom;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/* other getters and setters */
+
+	public void setEntity(LocalizedText entity) {
+		this.setId(entity.getId());
+		this.setModified(entity.getModified());
+		this.setModifiedBy(entity.getModifiedBy());
+		this.setCreated(entity.getCreated());
+		this.setCreatedBy(entity.getCreatedBy());
+		this.setFieldId(entity.getFieldId());
+		this.setLocale(entity.getLocale());
+		this.setValue(entity.getValue());
+	}
+
+	public String getDisplayValue() {
+		StringBuffer displayValue = new StringBuffer();
+		return displayValue.toString().trim();
+	}
+
+}
