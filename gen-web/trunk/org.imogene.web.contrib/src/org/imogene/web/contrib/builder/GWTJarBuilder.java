2529fde0bf0cc7dffad2e35417d8cb7ebe5155ff
medes.imps@gmail.com <medes.imps@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-05-18 17:13:06 +0000

    - Split the gilead4gwt in two jars : the first one contains the 'Gilead' libraries the second one the custom serializer for Date and Timestamp. This serializers are now provided by the GWT library and are in conflict with those. The 'compatibility' jar is used with the GWT libraries lower than 2.2. http://sourceforge.net/tracker/index.php?func=detail&aid=3285026&group_id=239931&atid=1111398
    - The builder check the GWT SDK version and sets the classpath with the appropriated Gilead libraries.

--- gen-web/trunk/org.imogene.web.contrib/src/org/imogene/web/contrib/builder/GWTJarBuilder.java
+++ gen-web/trunk/org.imogene.web.contrib/src/org/imogene/web/contrib/builder/GWTJarBuilder.java
@@ -3,44 +3,48 @@ package org.imogene.web.contrib.builder;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.imogene.library.contrib.export.ExportManager;
+import org.imogene.library.contrib.export.ExportedEntry;
 import org.imogene.studio.contrib.tools.FileHelper;
+import org.imogene.studio.contrib.ui.navigator.WebShadow;
+import org.imogene.web.contrib.Activator;
 
 import com.google.gwt.eclipse.core.runtime.GWTRuntime;
 
 @SuppressWarnings("restriction")
 public class GWTJarBuilder extends IncrementalProjectBuilder {
 	
-	private static final String CTX_OUT="war"; 
-
-	@SuppressWarnings( { "unchecked" })
+	
+	private static String LIBRARY_PATH = "war/WEB-INF/lib/";
+	
+	private static final String COMPATIBILITY_EXTENSION="2.1-compatibility";
+	
 	@Override
-	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
-			throws CoreException {
-		/*IJavaModel root = JavaCore.create(ResourcesPlugin.getWorkspace().getRoot());
-		IJavaProject project = root.getJavaProject(getProject().getName());*/
-		IJavaProject project = JavaCore.create(getProject());
-		//System.out.println("Running the builder");
+	protected IProject[] build(int kind, @SuppressWarnings("rawtypes") Map args, IProgressMonitor monitor)
+			throws CoreException {		
+		IJavaProject project = JavaCore.create(getProject());		
 		GWTRuntime runtime = GWTRuntime.findSdkFor(project);
 		for (File f : Arrays.asList(runtime.getWebAppClasspathFiles())) {		
-			IFolder library = (IFolder)project.getProject().findMember(CTX_OUT+"/WEB-INF/lib/");			
+			//IFolder library = (IFolder)project.getProject().findMember(CTX_OUT+"/WEB-INF/lib/");
+			IFolder library = (IFolder)project.getProject().findMember(LIBRARY_PATH);
 			File folder = new File(library.getLocation().toOSString());
 			File lib = new File(folder, f.getName());
-			//System.out.println("Src: "+f.getAbsolutePath());
-			//System.out.println("Dest: "+lib.getAbsolutePath());
-			//System.out.println("Should copy: "+shouldCopy(f, lib));
 			if(shouldCopy(f, lib)){
-				try{
-					
+				try{					
 					FileHelper.copyFile(f, new File(folder, f.getName()));
 				}catch (IOException ioe) {
 					ioe.printStackTrace();
@@ -48,7 +52,7 @@ public class GWTJarBuilder extends IncrementalProjectBuilder {
 				project.getProject().refreshLocal(IResource.DEPTH_INFINITE, null);				
 			}
 		}		
-
+		gileadCompatibility(runtime.getVersion(), getProject());
 		return null;
 	}
 	
@@ -58,6 +62,69 @@ public class GWTJarBuilder extends IncrementalProjectBuilder {
 		if(sdk.length()==lib.length())
 			return false;
 		return true;
+	}
+	
+	/**
+	 * Set the Gilead compatibility checking the SDK version
+	 * 
+	 * @param sdkVersion
+	 *            the GWT SDK version
+	 * @param project
+	 *            the WEB project
+	 * @throws CoreException
+	 */
+	private void gileadCompatibility(String sdkVersion, IProject project)
+			throws CoreException {	
+		List<ExportedEntry> entries = ExportManager
+				.getClasspath(WebShadow.NATURE);
+		IFolder iDestination = project.getFolder(LIBRARY_PATH);
+		for (ExportedEntry e : entries) {
+			try {
+				IFile iFile = iDestination.getFile(e.getFileName());
+				if (needCompatibility(sdkVersion)) {
+					if (!iFile.exists()
+							&& e.getFileName()
+									.contains(COMPATIBILITY_EXTENSION))
+						iFile.create(e.openStream(), true, null);
+				} else {
+					if (iFile.exists()
+							&& e.getFileName()
+									.contains(COMPATIBILITY_EXTENSION))
+						iFile.delete(true, null);
+				}
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+		project.refreshLocal(IResource.DEPTH_INFINITE, null);
+	}
+	
+	/**
+	 * check if the project need the 'Gilead' compatibility library
+	 * @param sdkVersion
+	 * @return
+	 */
+	private boolean needCompatibility(String sdkVersion){		
+		String[] versions = sdkVersion.split("\\.");
+		if(versions.length<2){
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Invalid sdk version number: '"+sdkVersion+"'"));
+			return false;			
+		}else{
+			try{
+				if(Integer.parseInt(versions[0])>1){
+					if(Integer.parseInt(versions[1])>1){
+						return false;
+					}else{
+						return true;
+					}
+				}else{
+					return true;
+				}
+			}catch(NumberFormatException nfe){
+				Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Invalid sdk version number: "+nfe.getMessage(), nfe));
+			}
+		}
+		return false;
 	}
 
 }
