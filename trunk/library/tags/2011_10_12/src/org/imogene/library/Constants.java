1edf1aa4e3413cd8d7520798f6a9b1d1256f9915
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-10-12 14:17:22 +0000

    -- Before Hibernate Mapping Update for Actors

---
+++ trunk/library/tags/2011_10_12/src/org/imogene/library/Constants.java
@@ -0,0 +1,18 @@
+package org.imogene.library;
+
+public class Constants {
+
+	public static final String JAR_PATH="libs/jars/";
+		
+	public static final String WEB_DESC_PATH="libs/webapp.xml";
+	
+	public static final String ADMIN_DESC_PATH="libs/webapp.xml";
+	
+	public static final String WS_DESC_PATH="libs/webservices.xml";
+	
+	public static final String SYNC_DESC_PATH="libs/synchro.xml";
+	
+	public static final String NOTIF_DESC_PATH="libs/notifier.xml";
+	
+	public static final String INIT_DESC_PATH="libs/initializer.xml";
+}
