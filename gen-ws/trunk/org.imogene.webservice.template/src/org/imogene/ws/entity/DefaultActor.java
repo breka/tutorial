50cb00faaaa87e2f3b373765d05887bf24742c4d
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-10-12 14:37:57 +0000

    -- Hibernate Mapping Update for Actors

--- gen-ws/trunk/org.imogene.webservice.template/src/org/imogene/ws/entity/DefaultActor.java
+++ gen-ws/trunk/org.imogene.webservice.template/src/org/imogene/ws/entity/DefaultActor.java
@@ -1,172 +1,232 @@
 package org.imogene.ws.entity;
 
 import java.util.Date;
-import java.util.Set;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
 
 public class DefaultActor implements MedooActor {
 	
-	private String login;	
-	private String id;	
-	private Date lastLoginDate;	
-	private String password;	
-	private Date creationDate;	
-	private String creator;	
-	private Date modificationDate;	
-	private String modifiedFrom;	
-	private Set<MedooRole> roles = new HashSet<MedooRole>();
+	/* Imog bean fields */
+	private String id;
+	private Date creationDate;
+	private String creator;
+	private Date lastModificationDate;
+	private String modifier;
+	private String modifiedFrom;
+	private Date uploadDate;
+	
+	/* Imog actor fields */
+	public static String ENTITY_NAME = "DefaultActor";
+	private String login;
+	private String password;
+	private String notifLocale;
+	private Integer defaultNotificationMethod;
+	private Boolean beNotified;
+	private HashMap<String, String> notificationData;
+	private Set<SynchronizableEntity> synchronizables;
+	private Set<MedooRole> assignedRoles = new HashSet<MedooRole>();
+	private Date lastLoginDate;
 	
 
-	public Date getLastLoginDate() {		
-		return lastLoginDate;
+	public DefaultActor() {
 	}
 
-	public String getLogin() {		
-		return login;
-	}
-	
-	public void setLogin(String pLogin){
-		login = pLogin;
-	}
+	/* Getters for Imog bean fields */
 
-
-	public String getPassword() {		
-		return password;
-	}
-
-
-	public void setLastLoginDate(Date date) {
-		lastLoginDate = date;		
-	}
-
-
-	public void setPassword(String pass) {		
-		password = pass;
-	}
-
-
-	public Date getCreationDate() {		
-		return creationDate;
-	}
-
-
-	public String getCreator() {		
-		return creator;
-	}
-
-
-	public String getDisplayValue() {		
-		return login;
-	}
-
-
-	public String getId() {		
+	public String getId() {
 		return id;
 	}
 
-
-	public Date getLastModificationDate() {		
-		return modificationDate;
+	public Date getCreationDate() {
+		return creationDate;
 	}
 
+	public String getCreator() {
+		return creator;
+	}
 
-	public String getModifiedFrom() {		
+	public Date getLastModificationDate() {
+		return lastModificationDate;
+	}
+
+	public String getModifier() {
+		return modifier;
+	}
+
+	public String getModifiedFrom() {
 		return modifiedFrom;
 	}
 
-
-	public String getModifier() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
 	public Date getUploadDate() {
-		
-		return null;
+		return uploadDate;
 	}
 
+	/* Setters for Imog bean fields */
 
-	public void setCreationDate(Date date) {	
+	public void setId(String pId) {
+		id = pId;
+	}
+
+	public void setCreationDate(Date date) {
 		creationDate = date;
 	}
 
-
-	public void setCreator(String creator) {
-		this.creator = creator;	
+	public void setCreator(String pCreator) {
+		creator = pCreator;
 	}
-
-
-	public void setId(String id) {		
-		this.id = id;
-	}
-
 
 	public void setLastModificationDate(Date date) {
-		modificationDate = date;
+		lastModificationDate = date;
 	}
 
+	public void setModifier(String pModifier) {
+		modifier = pModifier;
+	}
 
 	public void setModifiedFrom(String terminal) {
-		modifiedFrom = terminal;		
+		modifiedFrom = terminal;
 	}
-
-
-	public void setModifier(String modifier) {
-		//TODO do nothing	
-	}
-
 
 	public void setUploadDate(Date date) {
-		// TODO Auto-generated method stub		
+		uploadDate = date;
 	}
 
 
-	public Integer getDefaultNotificationMethod() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getDisplayValue() {
+		return id;
+	}
+	
+	/** 
+	 * Get the assigned roles for this actor. 
+	 * @return a list of role names (EMPTY_LIST if none). 	  
+	 */
+	public Set<MedooRole> getRoles() {
+		return assignedRoles;
 	}
 
+	/**
+	 * Set the list of roles assigned to this actor. 	 
+	 * @param roles the list of roles assigned. 
+	 */
+	public void setRoles(Set<MedooRole> roles) {
+		assignedRoles = roles;
+	}
 
+	/** 
+	 * Remove the specified role of this actor assigned roles
+	 */
+	public void removeRole(MedooRole role) {
+		assignedRoles.remove(role);
+	}
+
+	/** 
+	 * Get this actor login.
+	 *@return the actor login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/** 
+	 * Get the assigned password for this actor.
+	 *@return the actor password 
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Set the actor login.
+	 *@param pLogin the actor login.
+	 */
+	public void setLogin(String pLogin) {
+		login = pLogin;
+	}
+
+	/**
+	 * set the actor password
+	 *@param pPassword the actor password 
+	 */
+	public void setPassword(String pPassword) {
+		password = pPassword;
+	}
+
+	/** 
+	 * Get the notification locale ISO code to use for this actor
+	 *@return the locale ISO code
+	 */
 	public String getNotificationLocale() {
-		// TODO Auto-generated method stub
-		return null;
+		return notifLocale;
 	}
 
+	/** 
+	 * Set the notification locale ISO code
+	 *@param pLocale the locale ISO code
+	 */
+	public void setNotificationLocale(String locale) {
+		notifLocale = locale;
+	}
 
+	/** 
+	 * Get the notification method to use (if none : NO_METHOD). 
+	 * @return the notification method id
+	 */
+	public Integer getNotificationMethod() {
+		return defaultNotificationMethod;
+	}
+
+	/**
+	 * Set the notification method to use.
+	 *@param method the notification method
+	 */
+	public void setNotificationMethod(Integer method) {
+		defaultNotificationMethod = method;
+	}
+
+	/**
+	 * get th notification data (ie: phone number, email) 
+	 * for the specified notification method.
+	 * @param method the notification method
+	 * @return the data associated with the method
+	 */
+	public String getNotificationDataMethodName(String method) {
+		return (String) notificationData.get(method);
+	}
+
+	/** 
+	 * This actor can be notified ?
+	 *@return true if actor can be notified.
+	 */
 	public Boolean getBeNotified() {
-		// TODO Auto-generated method stub
-		return null;
+		return beNotified;
 	}
 
-
-	public void setDefaultnotificationMethod(Integer method) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * Set if this actor can be notified
+	 *@param notif true if it can be notified
+	 */
+	public void setBeNotified(Boolean notif) {
+		beNotified = notif;
 	}
 
-
-	public void setBeNotified(Boolean notify) {
-		// TODO Auto-generated method stub
-		
+	@Override
+	public Date getLastLoginDate() {
+		return lastLoginDate;
 	}
 
+	@Override
+	public void setLastLoginDate(Date date) {
+		this.lastLoginDate = date;
+	}
 
-	public void setNotificationLocale(String iso) {
-		// TODO Auto-generated method stub
-		
+	public void setSynchronizables(Set<SynchronizableEntity> syncs) {
+		synchronizables = syncs;
 	}
-	
-	
-	public void setRoles(Set<MedooRole> pRoles){
-		roles = pRoles;
+
+	public Set<SynchronizableEntity> getSynchronizables() {
+		return synchronizables;
 	}
-	
-	public Set<MedooRole> getRoles(){
-		return roles;
-	}
-	
-	
 	
 }
