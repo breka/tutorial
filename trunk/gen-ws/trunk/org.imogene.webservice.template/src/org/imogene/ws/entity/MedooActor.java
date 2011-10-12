17999794761c684eb954abd8ec2e0c0ea0637690
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-10-12 14:37:57 +0000

    -- Hibernate Mapping Update for Actors

--- trunk/gen-ws/trunk/org.imogene.webservice.template/src/org/imogene/ws/entity/MedooActor.java
+++ trunk/gen-ws/trunk/org.imogene.webservice.template/src/org/imogene/ws/entity/MedooActor.java
@@ -11,44 +11,55 @@ import java.util.Set;
  */
 public interface MedooActor extends MedooBean {
  
- /** Get the String representation of the bean, described by the MainFields*/
- public String getLogin();
- 
- /** Get the assigned password for this user */
- public String getPassword();
- 
- /** Set the assigned password for this user */
- public void setPassword(String password);
+	/** Get the assigned login for this user */
+	public String getLogin();
+	
+	/** Set the assigned login for this user */
+	public void setLogin(String login);
 
- /** Get the actor last login date */
- public Date getLastLoginDate();
+	/** Get the assigned password for this user */
+	public String getPassword();
 
- /** Set the actor last login date */
- public void setLastLoginDate(Date lastLoginDate);
- 
- /** Get the local ISO code used by this actor */
- public String getNotificationLocale();
- 
- /** Set the local ISO code used by this actor */
- public void setNotificationLocale(String iso);
- 
- /** get the default method used to notify this actor */
- public Integer getDefaultNotificationMethod();
- 
- /** Set the default method used to notify this actor */
- public void setDefaultnotificationMethod(Integer method);
- 
- /** Is the notification activated for this user */
- public Boolean getBeNotified();
- 
- /** Set if the notification is activated for this user */
- public void setBeNotified(Boolean notify);
- 
- /** Get the role associated to this user*/
- public Set<MedooRole> getRoles();
- 
- /** Set the roles associted to this user */
- public void setRoles(Set<MedooRole> pRoles);
+	/** Set the assigned password for this user */
+	public void setPassword(String password);
+
+	/** Get the actor last login date */
+	public Date getLastLoginDate();
+
+	/** Set the actor last login date */
+	public void setLastLoginDate(Date lastLoginDate);
+	
+	
+	/** Get the roles associated to this user */
+	public Set<MedooRole> getRoles();
+
+	/** Set the roles associated to this user */
+	public void setRoles(Set<MedooRole> pRoles);
+	
+	/** Get the synchronizable entities associated to this user */
+	public Set<SynchronizableEntity> getSynchronizables();
+	
+	/** Set the synchronizable entities associated to this user **/
+	public void setSynchronizables(Set<SynchronizableEntity> syncs);
+
+	/** Get the local ISO code used by this actor */
+	public String getNotificationLocale();
+
+	/** Set the local ISO code used by this actor */
+	public void setNotificationLocale(String iso);
+	
+	/** get the default method used to notify this actor */
+	public Integer getNotificationMethod();
+
+	/** Set the default method used to notify this actor */
+	public void setNotificationMethod(Integer method);	
+
+	/** Is the notification activated for this user */
+	public Boolean getBeNotified();
+
+	/** Set if the notification is activated for this user */
+	public void setBeNotified(Boolean notify);
+
  
 
 }
