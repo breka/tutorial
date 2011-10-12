7f8baa6c41b3913a3468ff307c5e94c4b531a83c
sbastier.medes@gmail.com <sbastier.medes@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2011-10-12 14:36:34 +0000

    -- Hibernate Mapping Update for Actors

--- trunk/gen-web/trunk/org.imogene.web.template/src/org/imogene/web/gwt/common/entity/ImogActor.java
+++ trunk/gen-web/trunk/org.imogene.web.template/src/org/imogene/web/gwt/common/entity/ImogActor.java
@@ -3,52 +3,66 @@ package org.imogene.web.gwt.common.entity;
 import java.util.Date;
 import java.util.Set;
 
+import org.imogene.web.gwt.client.sync.SynchronizableEntity;
 
 /**
- * This interface describe a Imogene actor, which is a 
- * Imogene bean that is an application user too.
+ * This interface describes an Imogene actor, which is an Imogene bean that is an
+ * application user too.
  * @author Medes-IMPS
  */
-public interface ImogActor extends ImogBean {
- 
- /** Get the String representation of the bean, described by the MainFields*/
- public String getLogin();
- 
- /** Get the assigned password for this user */
- public String getPassword();
- 
- /** Set the assigned password for this user */
- public void setPassword(String password);
+public interface ImogActor extends ImogBean {		
 
- /** Get the actor last login date */
- public Date getLastLoginDate();
+	/** Get the assigned login for this user */
+	public String getLogin();
+	
+	/** Set the assigned login for this user */
+	public void setLogin(String login);
 
- /** Set the actor last login date */
- public void setLastLoginDate(Date lastLoginDate);
- 
- /** Get the local ISO code used by this actor */
- public String getNotificationLocale();
- 
- /** Set the local ISO code used by this actor */
- public void setNotificationLacole(String iso);
- 
- /** get the default method used to notify this actor */
- public Integer getDefaultNotificationMethod();
- 
- /** Set the default method used to notify this actor */
- public void setDefaultnotificationMethod(Integer method);
- 
- /** Is the notification activated for this user */
- public Boolean getBeNotified();
- 
- /** Set if the notification is activated for this user */
- public void setBeNotified(Boolean notify);
- 
- /** Get the role associated to this user*/
- public Set<ImogRole> getRoles();
- 
- /** Set the roles associted to this user */
- public void setRoles(Set<ImogRole> pRoles);
- 
+	/** Get the assigned password for this user */
+	public String getPassword();
+
+	/** Set the assigned password for this user */
+	public void setPassword(String password);
+
+	/** Get the actor last login date */
+	public Date getLastLoginDate();
+
+	/** Set the actor last login date */
+	public void setLastLoginDate(Date lastLoginDate);
+	
+	
+	/** Get the roles associated to this user */
+	public Set<ImogRole> getRoles();
+
+	/** Set the roles associated to this user */
+	public void setRoles(Set<ImogRole> pRoles);
+	
+	/** Get the synchronizable entities associated to this user */
+	public Set<SynchronizableEntity> getSynchronizables();
+	
+	/** Set the synchronizable entities associated to this user **/
+	public void setSynchronizables(Set<SynchronizableEntity> syncs);
+
+	/** Get the local ISO code used by this actor */
+	public String getNotificationLocale();
+
+	/** Set the local ISO code used by this actor */
+	public void setNotificationLocale(String iso);
+	
+	/** get the default method used to notify this actor */
+	public Integer getNotificationMethod();
+
+	/** Set the default method used to notify this actor */
+	public void setNotificationMethod(Integer method);	
+
+	/** Is the notification activated for this user */
+	public Boolean getBeNotified();
+
+	/** Set if the notification is activated for this user */
+	public void setBeNotified(Boolean notify);
+
+
+
+
 
 }
