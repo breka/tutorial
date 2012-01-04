4a3e0abe1d6f45052641f6b55041d2e4bf24cd0e
julien.dupouy.fr@gmail.com <julien.dupouy.fr@gmail.com@ebdd1a65-156c-932d-9aad-d2100c70bdcf>
2012-01-04 17:36:54 +0000

    - compute tile folder size in a thread not to block the UI

--- trunk/gen-droid/trunk/org.imogene.android.template/src_maps/org/imogene/android/maps/preference/ClearCachePreference.java
+++ trunk/gen-droid/trunk/org.imogene.android.template/src_maps/org/imogene/android/maps/preference/ClearCachePreference.java
@@ -6,18 +6,25 @@ import org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants;
 
 import android.content.Context;
 import android.os.Handler;
+import android.os.Message;
 import android.preference.Preference;
 import android.util.AttributeSet;
 
 public class ClearCachePreference extends Preference {
 	
-	private final Handler mHandler = new Handler();
-	private final Runnable mUpdate = new Runnable() {
-		
-		@Override
-		public void run() {
-			notifyChanged();
-		}
+	private static final int MSG_UPDATE_ID = 1;
+	
+	private final Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case MSG_UPDATE_ID:
+				notifyChanged();
+				break;
+			default:
+				super.handleMessage(msg);
+				break;
+			}
+		};
 	};
 	
 	public ClearCachePreference(Context context, AttributeSet attrs) {
@@ -29,9 +36,14 @@ public class ClearCachePreference extends Preference {
 	}
 	
 	@Override
+	protected void onAttachedToActivity() {
+		super.onAttachedToActivity();
+		nonBlockingUpdate();
+	}
+	
+	@Override
 	public CharSequence getSummary() {
-		long size = FileUtils.getDirectorySize(OpenStreetMapTileProviderConstants.TILE_PATH_BASE);
-		String readableSize = FileUtils.readableFileSize(size);
+		String readableSize = FileUtils.readableFileSize(getPersistedLong(0));
 		return getContext().getString(R.string.ig_precache_clear_summary, readableSize);
 	}
 	
@@ -41,9 +53,24 @@ public class ClearCachePreference extends Preference {
 			@Override
 			public void run() {
 				FileUtils.deleteDirectory(OpenStreetMapTileProviderConstants.TILE_PATH_BASE);
-				mHandler.post(mUpdate);
+				blockingUpdate();
 			}
 		}).start();
+	}
+
+	private void nonBlockingUpdate() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				blockingUpdate();
+			}
+		}).start();
+	}
+	
+	private void blockingUpdate() {
+		long size = FileUtils.getDirectorySize(OpenStreetMapTileProviderConstants.TILE_PATH_BASE);
+		persistLong(size);
+		mHandler.sendEmptyMessage(MSG_UPDATE_ID);
 	}
 
 }
